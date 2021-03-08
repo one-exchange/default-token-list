09b756dd697963c4239d99dd49b55f270c32d067
SebastianJ <sebastian.johnsson@gmail.com>
2021-03-08 18:37:07 +0100

    First version

--- src/buildList.js
+++ src/buildList.js
@@ -4,11 +4,13 @@ const ropsten = require("./tokens/ropsten.json");
 const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
 const kovan = require("./tokens/kovan.json");
+const harmonyMainnet = require("./tokens/harmony_mainnet.json");
+const harmonyTestnet = require("./tokens/harmony_testnet.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Uniswap Default List",
+    name: "ExchangeOne Default",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -18,7 +20,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
     keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby]
+    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...harmonyMainnet, ...harmonyTestnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
